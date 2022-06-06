3e5c424c152ed8cf255fbadf25f11ad92e41caf4
Kheang Hok Chin <31987945+simplecommerce@users.noreply.github.com>
2022-06-06 09:11:09 -0400

    Update Info.jsx
    demonstrate standby issue

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -3,16 +3,19 @@ import { gql, useQuery, useLazyQuery, useMutation } from '@apollo/client';
 import { map, get } from "lodash";
 
 export const Info = () => {
-//   const { data, error, called, networkStatus } = useQuery(gql`
+    const { data, error, called, networkStatus } = useQuery(gql`
+    query GetProjects {
+      getProjects 
+    }
+     `,{
+         // commenting this, the page will load normally
+         fetchPolicy: "standby"
+     });
+//   const [fetch,{ data, error, called, networkStatus }] = useLazyQuery(gql`
 //  query GetProjects {
 //    getProjects 
 //  }
-//   `);
-  const [fetch,{ data, error, called, networkStatus }] = useLazyQuery(gql`
- query GetProjects {
-   getProjects 
- }
-  `);    
+//   `);  
 
   console.log(data, called, error, networkStatus)
 
